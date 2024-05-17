8ef8f7adf06bdb0abe9a86b97d82cac7125d2f3f
Daniel Dias <daniel.dias@ludustechnology.com>
2024-05-17 15:31:32 +0100

    Added a few smoth animations to the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,38 @@ import Navbar from './Components/Navbar'
 
 function App() {
   return (
-    <div>
-      <Navbar></Navbar>
-      <h1 className="text-3xl font-bold">
-      </h1>
+    <div className="flex h-screen">
+      <Navbar />
+      <div className="w-full font-serif overflow-auto">
+        <section className="h-screen flex items-center justify-center text-white">
+          <div>
+            <h1 className='text-center text-2xl'>About me</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+        </section>
+        <section className="h-screen flex items-center justify-center text-white">
+          <div>
+            <h1 className='text-center text-2xl'>Skills</h1>
+          </div>
+        </section>
+        <section className="h-screen flex items-center justify-center text-white">
+          <div>
+            <h1 className='text-center text-2xl'>Experience</h1>
+          </div>
+        </section>
+        <section className="h-screen flex items-center justify-center text-white">
+          <div>
+            <h1 className='text-center text-2xl'>Projects</h1>
+          </div>
+        </section>
+        <section className="h-screen flex items-center justify-center text-white">
+          <div>
+            <h1 className='text-center text-2xl'>Contacts</h1>
+          </div>
+        </section>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 function Navbar() {
     return (
-        <div className="h-screen w-1/3 md:w-1/4 lg:w-1/5 flex flex-col justify-center items-center bg-zinc-800 text-white">
+        <div className="h-screen stick top-0 w-1/3 md:w-1/4 lg:w-1/5 flex flex-col justify-center items-center text-white ">
             <div className="space-y-4 flex flex-col text-xl md:text-2xl font-serif gap-y-4">
-                <a href="" className="hover:text-gray-300">About Me</a>
-                <a href="" className="hover:text-gray-300">Experience</a>
-                <a href="" className="hover:text-gray-300">Projects</a>
-                <a href="" className="hover:text-gray-300">Contacts</a>
+                <a href="" className="hover:text-gray-300 transition-transform transform hover:scale-110">About Me</a>
+                <a href="" className="hover:text-gray-300 transition-transform transform hover:scale-110">Experience</a>
+                <a href="" className="hover:text-gray-300 transition-transform transform hover:scale-110">Projects</a>
+                <a href="" className="hover:text-gray-300 transition-transform transform hover:scale-110">Contacts</a>
             </div>
         </div>
     );
