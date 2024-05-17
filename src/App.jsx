a6b19a396f2a8dbaa2e9c048a803d52f2f8a363e
Daniel Dias <daniel.dias@ludustechnology.com>
2024-05-17 16:10:46 +0100

    Changed CSS on each section

--- src/App.jsx
+++ src/App.jsx
@@ -5,31 +5,45 @@ function App() {
   return (
     <div className="flex h-screen">
       <Navbar />
-      <div className="w-full font-serif overflow-auto">
-        <section className="h-screen flex items-center justify-center text-white">
-          <div>
-            <h1 className='text-center text-2xl'>About me</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <div className="w-full font-serif overflow-auto p-5">
+        <section className="h-screen flex flex-col items-center justify-center text-white">
+          <h1 className='text-center text-4xl'>About me</h1>
+          <div >
+            <div className='text-l md:text-2xl'>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
           </div>
         </section>
         <section className="h-screen flex items-center justify-center text-white">
-          <div>
-            <h1 className='text-center text-2xl'>Skills</h1>
+          <div >
+            <h1 className='text-center text-4xl'>Skills</h1>
+            <div className='text-l md:text-2xl'>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
           </div>
         </section>
         <section className="h-screen flex items-center justify-center text-white">
-          <div>
-            <h1 className='text-center text-2xl'>Experience</h1>
+          <div >
+            <h1 className='text-center text-4xl'>Experience</h1>
+            <div className='text-l md:text-2xl'>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
           </div>
         </section>
         <section className="h-screen flex items-center justify-center text-white">
-          <div>
-            <h1 className='text-center text-2xl'>Projects</h1>
+          <div >
+            <h1 className='text-center text-4xl'>Projects</h1>
+            <div className='text-l md:text-2xl'>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
           </div>
         </section>
         <section className="h-screen flex items-center justify-center text-white">
-          <div>
-            <h1 className='text-center text-2xl'>Contacts</h1>
+          <div >
+            <h1 className='text-center text-4xl'>Contacts</h1>
+            <div className='text-l md:text-2xl'>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
           </div>
         </section>
       </div>
