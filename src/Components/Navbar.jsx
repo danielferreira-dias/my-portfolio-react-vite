0bd05bbf345f33d6c2316ff606bb2b8ad1109eb1
Daniel Dias <daniel.dias@ludustechnology.com>
2024-05-22 12:52:34 +0100

    small change

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -14,7 +14,7 @@ function Navbar() {
             </div>
             <div className="hidden sm:flex items-center justify-end space-x-2 ml-10">
                 <button className="bg-zinc-800 px-2 text-white rounded w-fit h-12">
-                    <img src="moon.svg" alt="" className='h-8' />
+                    <img src="moon.svg" alt="" className='h-7' />
                 </button>
                 <button className="bg-zinc-800 text-white px-4 py-2 rounded w-fit h-12">
                     <p>Resume</p>
