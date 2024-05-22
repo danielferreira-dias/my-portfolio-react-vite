50877b51b3892641ea75811efd1da02b5668be42
Daniel Dias <daniel.dias@ludustechnology.com>
2024-05-22 10:54:56 +0100

    Deleted code to reset layout

--- src/App.jsx
+++ src/App.jsx
@@ -5,62 +5,11 @@ import React, { useState } from 'react';
 
 function App() {
 
-  const [activeSection, setActiveSection] = useState('');
 
-  const { ref: aboutMeRef, inView: isAboutMeInView } = useInView({ threshold: 0.5, onChange: inView => inView && setActiveSection('aboutme') });
-  const { ref: skillsRef, inView: isSkillsInView } = useInView({ threshold: 0.5, onChange: inView => inView && setActiveSection('skills') });
-  const { ref: experienceRef, inView: isExperienceInView } = useInView({ threshold: 0.5, onChange: inView => inView && setActiveSection('experience') });
-  const { ref: projectsRef, inView: isProjectsInView } = useInView({ threshold: 0.5, onChange: inView => inView && setActiveSection('projects') });
-  const { ref: contactsRef, inView: isContactsInView } = useInView({ threshold: 0.5, onChange: inView => inView && setActiveSection('contacts') });
 
   return (
-    <div className="flex h-screen">
-      <Navbar activeSection={activeSection} />
-      <div className="w-full font-serif overflow-auto p-5">
-        <section id="aboutme" ref={aboutMeRef} className="h-screen flex flex-col items-center justify-center gap-y-5 text-white">
-          <div className="w-36 h-36 sm:w-44 sm:h-44 lg:w-64 lg:h-64 rounded-full overflow-hidden">
-            <img src="profile.jpg" alt="Profile Image" className="w-full h-full object-cover" />
-          </div>
-          <h1 className='text-center text-4xl'>About me</h1>
-          <div>
-            <div className='text-l md:text-2xl'>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </div>
-          </div>
-        </section>
-        <section id="skills" ref={skillsRef} className="h-screen flex items-center justify-center text-white">
-          <div >
-            <h1 className='text-center text-4xl'>Skills</h1>
-            <div className='text-l md:text-2xl'>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </div>
-          </div>
-        </section>
-        <section id="experience" ref={experienceRef} className="h-screen flex items-center justify-center text-white">
-          <div >
-            <h1 className='text-center text-4xl'>Experience</h1>
-            <div className='text-l md:text-2xl'>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </div>
-          </div>
-        </section>
-        <section id="projects" ref={projectsRef} className="h-screen flex items-center justify-center text-white">
-          <div >
-            <h1 className='text-center text-4xl'>Projects</h1>
-            <div className='text-l md:text-2xl'>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </div>
-          </div>
-        </section>
-        <section id="contacts" ref={contactsRef} className="h-screen flex items-center justify-center text-white">
-          <div >
-            <h1 className='text-center text-4xl'>Contacts</h1>
-            <div className='text-l md:text-2xl'>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </div>
-          </div>
-        </section>
-      </div>
+    <div className="flex h-screen px-10 py-5">
+      <Navbar></Navbar>
     </div>
   );
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,21 +1,16 @@
 import React from 'react'
 
-function Navbar({ activeSection }) {
-    const scrollToSection = (sectionId) => {
-        const section = document.getElementById(sectionId);
-        if (section) {
-            section.scrollIntoView({ behavior: 'smooth', block: 'start' });
-        }
-    };
+function Navbar() {
 
     return (
-        <div className="h-screen stick top-0 w-1/3 md:w-1/4 lg:w-1/5 flex flex-col justify-center items-center text-white ">
-            <div className="space-y-4 flex flex-col text-xl md:text-2xl font-serif gap-y-4">
-                <a href="#aboutme" onClick={() => scrollToSection('aboutme')} className={`text-lg md:text-xl hover:text-gray-300 ${activeSection === 'aboutme' ? 'transition-transform transform scale-125' : ''}`}>About Me</a>
-                <a href="#skills" onClick={() => scrollToSection('skills')} className={`text-lg md:text-xl hover:text-gray-300 ${activeSection === 'skills' ? 'transition-transform transform scale-125' : ''}`}>Skills</a>
-                <a href="#experience" onClick={() => scrollToSection('experience')} className={`text-lg md:text-xl hover:text-gray-300 ${activeSection === 'experience' ? 'transition-transform transform scale-125' : ''}`}>Experience</a>
-                <a href="#projects" onClick={() => scrollToSection('projects')} className={`text-lg md:text-xl hover:text-gray-300 ${activeSection === 'projects' ? 'transition-transform transform scale-125' : ''}`}>Projects</a>
-                <a href="#contacts" onClick={() => scrollToSection('contacts')} className={`text-lg md:text-xl hover:text-gray-300 ${activeSection === 'contacts' ? 'transition-transform transform scale-125' : ''}`}>Contacts</a>
+        <div className="h-20  w-full flex justify-between items-center p-5 text-white">
+            <div className="flex-grow">
+                <img src="react.svg" alt="" className="w-10 h-auto" />
+            </div>
+            <div className="flex-grow flex justify-center">
+                <a href="" className="mx-2">About</a>
+                <a href="" className="mx-2">Portfolio</a>
+                <a href="" className="mx-2">Contact</a>
             </div>
         </div>
     );
