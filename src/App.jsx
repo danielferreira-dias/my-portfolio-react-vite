b66ed30a33992cad6408cce84d3ce2ed5b1d92cf
Daniel Dias <daniel.dias@ludustechnology.com>
2024-06-03 14:36:45 +0100

    Added Profile Picture

--- src/App.jsx
+++ src/App.jsx
@@ -12,18 +12,24 @@ function App() {
   };
 
   return (
-    <div className="flex flex-col h-screen px-5 sm:px-10 xl:px-40 2xl:px-80 py-5">
+    <div className="flex flex-col h-screen px-5 sm:px-10 xl:px-40 2xl:px-60 py-5">
       <Navbar onDataFromChild={handleDataFromChild}></Navbar>
       <section className="w-full h-svh bg-white">
         <div className="w-full h-full justify-between xs:justify-normal flex flex-col p-5">
           {/* Introduction Div */}
-          <div className='flex items-start xs:items-center justify-center xs:justify-normal h-fit xs:h-full mx-5 my-5 lg:mx-10'>
+          <div className='flex items-start xs:items-center justify-center xs:justify-between h-fit xs:h-full mx-5 my-5 lg:mx-10 flex-col xs:flex-row'>
             <div className='flex flex-col gap-y-4 my-10 xs:my-0'>
               <p className="text-xl sm:text-2xl lg:text-5xl">I'm <span className="text-2xl sm:text-3xl lg:text-6xl text-primary">Daniel Dias</span></p>
               <p className="text-lg sm:text-xl lg:text-3xl">Full Stack Web Developer</p>
               <button className='w-40 text-sm sm:text-xl border-4 rounded border-primary text-primary p-2'>Contact Me</button>
             </div>
+
+            {/* Image Div */}
+            <div className="relative h-fit w-42  xs:flex xs:w-60 md:w-72 lg:w-80 xl:w-96 xs:ml-5">
+              <img src="profile.jpg" alt="Profile" className=" rounded border-2" />
+            </div>
           </div>
+
           {/* Symbol Div */}
           <div className="relative h-fit">
             <div className="flex flex-row justify-center xs:justify-normal xs:absolute bottom-0 right-0 xs:flex-col items-center gap-5">
@@ -32,6 +38,7 @@ function App() {
               <img src="github.svg" alt="" className="w-10 h-10" />
             </div>
           </div>
+
         </div>
       </section>
     </div>
