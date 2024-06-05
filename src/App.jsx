65c23762f1006b1856860ebe789ac54825a3973f
Daniel Dias <daniel.dias@ludustechnology.com>
2024-06-05 15:30:48 +0100

    Changed svgs, background color

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,17 @@ import React, { useState, useEffect } from 'react';
 function App() {
 
   const [dataFromChild, setDataFromChild] = useState(true);
+  const [linkedinMode, setLinkedinMode] = useState('linkedin-light.svg');; // Dark theme SVG Linkedin
+  const linkedinDark = 'linkedin-dark.svg'
+
+
+  const [gitHubMode, setGitHubMode] = useState('github-light.svg'); // Dark theme SVG Gitub
+  const githubDark = 'github-dark.svg'
 
   const handleDataFromChild = (data) => {
     setDataFromChild(data);
+    setLinkedinMode(dataFromChild ? linkedinDark : 'linkedin-light.svg');
+    setGitHubMode(dataFromChild ? githubDark : 'github-light.svg');
   };
 
   return (
@@ -35,25 +43,29 @@ function App() {
           <div className="relative h-fit">
             <div className="flex flex-row justify-center xs:justify-normal xs:absolute bottom-0 right-0 xs:flex-col items-center gap-5">
               <span className="w-1 h-4 xs:h-16 bg-primary rotate-90 xs:rotate-0"></span>
-              <img src="linkedin-dark.svg" alt="" className="w-10 h-5" />
-              <img src="github.svg" alt="" className="w-10 h-10" />
+              <img src={linkedinMode} alt="" className="w-10 h-5" />
+              <img src={gitHubMode} alt="" className="w-10 h-10" />
             </div>
           </div>
 
         </div>
       </section>
 
-      <section className={`w-full h-fit bg-white ${dataFromChild ? ' transition-colors duration-300 ease-in-out text-white' : ' transition-colors duration-300 ease-in-out text-black'}`}>
-        <h1 className='text-2xl text-black'>About</h1>
-        <div className='flex flex-row mt-2'>
-          <div className="flex mb-4 mt-3">
+      <section className={`w-full h-fit my-5  ${dataFromChild ? ' transition-colors duration-300 ease-in-out text-white' : ' transition-colors duration-300 ease-in-out text-black'}`}>
+        <h1 className='text-2xl xl:text-4xl '>About</h1>
+        <div className='flex flex-row mt-4'>
+          <div className="flex mt-3 ">
             <span className="block w-8 h-1 bg-primary mr-2"></span>
           </div>
-          <p className="text-lg leading-relaxed text-black text-left">
+          <p className="text-lg lg:text-xl xl:text-2xl leading-relaxed  text-left">
             I have acquired and sharpened my ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum vn organization. I am <span className="text-primary font-semibold">trustworthy, creative, and an effective communicator</span>. I fondly anticipate working in a challenging yet rewarding organization to attain its visions for personal growth.
           </p>
         </div>
       </section>
+
+      <section className={`w-full h-fit my-5 bg-white  ${dataFromChild ? ' transition-colors duration-300 ease-in-out text-white' : ' transition-colors duration-300 ease-in-out text-black'}`}>
+      </section>
+
     </div>
   );
 }
