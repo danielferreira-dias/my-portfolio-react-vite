6b20c68bcd2f30b165d43b66ec9e8fdb07884778
Daniel Dias <daniel.dias@ludustechnology.com>
2024-06-06 12:22:24 +0100

    Small fix

--- src/Components/SkillBox.jsx
+++ src/Components/SkillBox.jsx
@@ -2,7 +2,7 @@ import '../index.css'
 
 const Skillbox = ({ skillName, iconSrc, isDarkMode }) => {
     return (
-        <div className={`flex items-center relative rounded-lg w-32 h-14 lg:w-40 2xl:w-60 overflow-hidden shadow-md my-2 ${isDarkMode ? 'bg-gradient-to-r from-black to-blue-900 shadow-blue-900 hover:shadow-blue-700' : 'bg-gradient-to-r from-white to-gray-300 shadow-gray-200 hover:shadow-gray-100'}`}>
+        <div className={`flex items-center relative rounded-lg w-28 h-14 xs:w-32 lg:w-40 2xl:w-60 overflow-hidden shadow-md my-2 ${isDarkMode ? 'bg-gradient-to-r from-black to-blue-900 shadow-blue-900 hover:shadow-blue-700' : 'bg-gradient-to-r from-white to-gray-300 shadow-gray-200 hover:shadow-gray-100'}`}>
             <div className='flex items-center h-full ml-5'>
                 <span className={`text-lg lg:text-xl 2xl:text-2xl ${isDarkMode ? 'text-white' : 'text-black'}`}>{skillName}</span>
             </div>
