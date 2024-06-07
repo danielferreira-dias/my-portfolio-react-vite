bf4d3722b065f09343cacd402b610ebf58ac83cd
Daniel Dias <daniel.dias@ludustechnology.com>
2024-06-07 17:27:35 +0100

    Update experience section

--- src/App.jsx
+++ src/App.jsx
@@ -161,10 +161,10 @@ function App() {
       </section>
 
       <section className={`w-full h-fit my-8 ${dataFromChild ? ' transition-colors duration-300 ease-in-out text-white' : ' transition-colors duration-300 ease-in-out text-black'}`}>
-        <div className='px-5 sm:px-10 xl:px-40 2xl:px-64'>
+        <div className='px-5 sm:px-10 xl:px-40 2xl:px-64 md:hidden'>
           <h1 className='text-2xl xl:text-4xl '>Experience</h1>
 
-          <div className='flex flex-row w-full gap-8'>
+          <div className='flex flex-row w-full gap-8 '>
             <div class="relative w-fit h-64 mt-10 md:top-1/2">
               <div class="absolute left-1/2 transform -top-1 -translate-x-1/2 w-4 h-4 border-2 border-blue-950 rounded-full"></div>
               <div class="absolute left-1/2 transform top-0 -translate-x-1/2 w-2 h-2 bg-blue-950 rounded-full"></div>
@@ -190,11 +190,57 @@ function App() {
             </div>
           </div>
 
-
+          <div className='flex flex-row w-full gap-8'>
+            <div class="relative w-fit h-64">
+              <div class="absolute left-1/2 transform -top-1 -translate-x-1/2 w-4 h-4 border-2 border-blue-950 rounded-full"></div>
+              <div class="absolute left-1/2 transform top-0 -translate-x-1/2 w-2 h-2 bg-blue-950 rounded-full"></div>
+              <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-0.5 h-full bg-blue-950"></div>
+            </div>
+            <div className=' h-64 flex flex-col'>
+              <h1 className='text-xl'>Universidade da Maia</h1>
+              <p>06/09/2021 - Present</p>
+              <p className='mt-2'> I have acquired and sharpened my ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum vn organization. I am <span className="text-primary font-semibold">trustworthy, creative, and an effective communicator</span>. I fondly anticipate working in a challenging yet rewarding organization to attain its visions for personal growth. </p>
+            </div>
+          </div>
         </div>
-      </section>
 
-    </div>
+        <div className='px-5 sm:px-10 xl:px-40 2xl:px-64 h-fit w-full hidden md:block'>
+          <h1 className='text-2xl xl:text-4xl '>Experience</h1>
+
+          <div className='flex flex-row gap-8 '>
+            <div className='mt-10 md:top-1/2 h-64 flex flex-col w-1/2'>
+              <h1 className='text-xl'>Luduscristaltec</h1>
+              <p>06/09/2021 - Present</p>
+              <p className='mt-2 overflow-hidden'> I have acquired and sharpened my ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum vn organization. I am <span className="text-primary font-semibold">trustworthy, creative, and an effective communicator</span>. I fondly anticipate working in a challenging yet rewarding organization to attain its visions for personal growth. </p>
+            </div>
+            <div class="relative w-fit h-64 mt-10 md:top-1/2">
+              {/* Circle Line */}
+              <div class="absolute left-1/2 transform -top-1 -translate-x-1/2 w-4 h-4 border-2 border-blue-950 rounded-full"></div>
+              <div class="absolute left-1/2 transform top-0 -translate-x-1/2 w-2 h-2 bg-blue-950 rounded-full"></div>
+              <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-0.5 h-full bg-blue-950"></div>
+            </div>
+          </div>
+
+          <div className='flex flex-row gap-8'>
+            <div class="relative w-fit h-64 mt-10 md:top-1/2">
+              {/* Circle Line */}
+              <div class="absolute left-1/2 transform -top-1 -translate-x-1/2 w-4 h-4 border-2 border-blue-950 rounded-full"></div>
+              <div class="absolute left-1/2 transform top-0 -translate-x-1/2 w-2 h-2 bg-blue-950 rounded-full"></div>
+              <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-0.5 h-full bg-blue-950"></div>
+            </div>
+            <div className='mt-10 md:top-1/2 h-64 flex flex-col w-1/2'>
+              <h1 className='text-xl'>Luduscristaltec</h1>
+              <p>06/09/2021 - Present</p>
+              <p className='mt-2 overflow-hidden'> I have acquired and sharpened my ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum vn organization. I am <span className="text-primary font-semibold">trustworthy, creative, and an effective communicator</span>. I fondly anticipate working in a challenging yet rewarding organization to attain its visions for personal growth. </p>
+            </div>
+          </div>
+        </div>
+
+
+
+      </section >
+
+    </div >
   );
 }
 
