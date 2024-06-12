744386b9b52cdb922df3703b09f6e3a6c635d4e3
Daniel Dias <danieldias.it98@gmail.com>
2024-06-12 16:57:55 +0100

    Small change to animation

--- src/Sections/Experience.jsx
+++ src/Sections/Experience.jsx
@@ -8,9 +8,7 @@ const Experience = () => {
 
                 <div className='flex flex-row w-full gap-8 h-fit'>
                     <div className="relative w-fit mt-10 md:top-1/2  xs:h-96 sm:h-80">
-                        <div className="absolute left-1/2 transform -top-1 -translate-x-1/2 w-4 h-4 border-2 border-blue-950 rounded-full">
-                            <div className="absolute w-4 h-4 border-2 -top-0.5 -left-0.5 border-blue-950 rounded-full animate-scaleAndFade"></div>
-                        </div>
+                        <div className="absolute w-4 h-4 border-2 -top-1 -left-2 border-blue-950 rounded-full animate-scaleAndFade"></div>
                         <div className="absolute left-1/2 transform top-0 -translate-x-1/2 w-2 h-2 bg-blue-950 rounded-full"></div>
                         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-0.5 h-full bg-blue-950"></div>
                     </div>
@@ -28,9 +26,7 @@ const Experience = () => {
 
                 <div className='flex flex-row w-full gap-8 h-fit'>
                     <div className="relative w-fit  xs:h-96 sm:h-80">
-                        <div className="absolute left-1/2 transform -top-1 -translate-x-1/2 w-4 h-4 border-2 border-blue-950 rounded-full">
-                            <div className="absolute w-4 h-4 border-2 -top-0.5 -left-0.5 border-blue-950 rounded-full animate-scaleAndFade"></div>
-                        </div>
+                        <div className="absolute w-4 h-4 border-2 -top-1 -left-2 border-blue-950 rounded-full animate-scaleAndFade"></div>
                         <div className="absolute left-1/2 transform top-0 -translate-x-1/2 w-2 h-2 bg-blue-950 rounded-full"></div>
                         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-0.5 h-full bg-blue-950"></div>
                     </div>
@@ -48,9 +44,7 @@ const Experience = () => {
 
                 <div className='flex flex-row w-full gap-8 h-fit'>
                     <div className="relative w-fit xs:h-96 sm:h-80">
-                        <div className="absolute left-1/2 transform -top-1 -translate-x-1/2 w-4 h-4 border-2 border-blue-950 rounded-full">
-                            <div className="absolute w-4 h-4 border-2 -top-0.5 -left-0.5 border-blue-950 rounded-full animate-scaleAndFade"></div>
-                        </div>
+                        <div className="absolute w-4 h-4 border-2 -top-1 -left-2 border-blue-950 rounded-full animate-scaleAndFade"></div>
                         <div className="absolute left-1/2 transform top-0 -translate-x-1/2 w-2 h-2 bg-blue-950 rounded-full"></div>
                         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-0.5 h-full bg-blue-950"></div>
                     </div>
@@ -83,9 +77,7 @@ const Experience = () => {
                             </div>
                         </div>
                         <div className="relative w-fit ">
-                            <div className="absolute left-1/2 transform -top-1 -translate-x-1/2 w-4 h-4 border-2 border-blue-950 rounded-full">
-                                <div className="absolute w-4 h-4 border-2 -top-0.5 -left-0.5 border-blue-950 rounded-full animate-scaleAndFade"></div>
-                            </div>
+                            <div className="absolute w-4 h-4 border-2 -top-1 -left-2 border-blue-950 rounded-full animate-scaleAndFade"></div>
                             <div className="absolute left-1/2 transform top-0 -translate-x-1/2 w-2 h-2 bg-blue-950 rounded-full"></div>
                             <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-0.5 h-full bg-blue-950"></div>
                         </div>
@@ -95,9 +87,7 @@ const Experience = () => {
                 <div className='flex flex-row justify-end gap-8'>
                     <div className='flex flex-row w-1/2  h-96 lg:h-80 '>
                         <div className="relative w-fit ">
-                            <div className="absolute left-1/2 transform -top-1 -translate-x-1/2 w-4 h-4 border-2 border-blue-950 rounded-full">
-                                <div className="absolute w-4 h-4 border-2 -top-0.5 -left-0.5 border-blue-950 rounded-full animate-scaleAndFade"></div>
-                            </div>
+                            <div className="absolute w-4 h-4 border-2 -top-1 -left-2 border-blue-950 rounded-full animate-scaleAndFade"></div>
                             <div className="absolute left-1/2 transform top-0 -translate-x-1/2 w-2 h-2 bg-blue-950 rounded-full"></div>
                             <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-0.5 h-full bg-blue-950"></div>
                         </div>
@@ -127,9 +117,7 @@ const Experience = () => {
                             </div>
                         </div>
                         <div className="relative w-fit h-96 lg:h-80">
-                            <div className="absolute left-1/2 transform -top-1 -translate-x-1/2 w-4 h-4 border-2 border-blue-950 rounded-full">
-                                <div className="absolute w-4 h-4 border-2 -top-0.5 -left-0.5 border-blue-950 rounded-full animate-scaleAndFade"></div>
-                            </div>
+                            <div className="absolute w-4 h-4 border-2 -top-1 -left-2 border-blue-950 rounded-full animate-scaleAndFade"></div>
                             <div className="absolute left-1/2 transform top-0 -translate-x-1/2 w-2 h-2 bg-blue-950 rounded-full"></div>
                             <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-0.5 h-full bg-blue-950"></div>
                         </div>
