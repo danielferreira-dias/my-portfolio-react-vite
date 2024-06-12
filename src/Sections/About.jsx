0b74694bbc3a5c26df7ea5b87adf8d7775d697f8
Daniel Dias <danieldias.it98@gmail.com>
2024-06-12 13:05:27 +0100

    Test

--- src/Sections/About.jsx
+++ src/Sections/About.jsx
@@ -9,7 +9,7 @@ const About = ({ isDarkMode }) => {
                     <span className="block w-8 h-1 bg-primary mr-2"></span>
                 </div>
                 <p className="text-lg lg:text-xl xl:text-2xl leading-relaxed  text-left">
-                    I have acquired and sharpened my ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum vn organization. I am <span className="text-primary font-semibold">trustworthy, creative, and an effective communicator</span>. I fondly anticipate working in a challenging yet rewarding organization to attain its visions for personal growth.
+                    Teste I have acquired and sharpened my ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum vn organization. I am <span className="text-primary font-semibold">trustworthy, creative, and an effective communicator</span>. I fondly anticipate working in a challenging yet rewarding organization to attain its visions for personal growth.
                 </p>
             </div>
         </div>
