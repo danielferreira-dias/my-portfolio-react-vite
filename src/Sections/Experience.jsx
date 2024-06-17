b160a2112794e69e0cd5d7358108cab69292cb05
Daniel Dias <danieldias.it98@gmail.com>
2024-06-17 17:50:52 +0100

    Updated experience section for smaller devices

--- src/Sections/Experience.jsx
+++ src/Sections/Experience.jsx
@@ -68,7 +68,7 @@ const Experience = ({ isDarkMode }) => {
                 <h1 className='text-2xl xl:text-4xl mb-10'>Experience</h1>
 
                 <div className='flex flex-row w-full gap-8 h-fit'>
-                    <div className="relative w-fit mt-10 md:top-1/2  xs:h-96 sm:h-80">
+                    <div className="relative w-fit mt-10 md:top-1/2 ">
                         <div className="absolute w-4 h-4 border-2 -top-1 -left-2 border-blue-950 rounded-full animate-scaleAndFade"></div>
                         <div className="absolute left-1/2 transform top-0 -translate-x-1/2 w-2 h-2 bg-blue-950 rounded-full"></div>
                         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-0.5 h-full bg-blue-950"></div>
@@ -78,10 +78,17 @@ const Experience = ({ isDarkMode }) => {
                             <h1 className='text-xl'>Luduscristaltec</h1>
                             <p>06/09/2021 - Present</p>
                         </div>
-                        <p className='mt-2'> I have acquired and sharpened my ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum vn organization. I am <span className="text-primary font-semibold">trustworthy, creative, and an effective communicator</span>. I fondly anticipate working in a challenging yet rewarding organization to attain its visions for personal growth. </p>
-                        <div className='flex flex-col w-full h-fit '>
-                            <p className='font-bold'>Technologies</p>
-                            <div className='flex flex-row flex-wrap w-full h-9  p-0.5 gap-2'>
+                        <div className='flex flex-col gap-y-2'>
+                            <p className='mt-2'> Primarily, I developed the game feed which is an integral component that offers users a view of previous gameplay.
+                                Further more, I also developed an extensive "info" section. This section serves as a guide to the many features of the
+                                game to better understanding of the user. Utilizing <span className="text-primary font-semibold">front-end technologies</span>, I integrated frontend frameworks such as React.js.
+                                This integration was important in creating an intuitive, user-friendly interface.
+                            </p>
+                            <p>I also specialize in <span className="text-primary font-semibold"> developing casino slot games for both mobile and laptop platforms </span> using the
+                                company proprietary language, which is rooted in C.</p>
+                        </div>                        <div className='flex flex-col w-full h-fit '>
+                            <p className='font-bold mt-2'>Technologies</p>
+                            <div className='flex flex-row flex-wrap w-full h-9  p-0.5 gap-2 mt-2'>
                                 {thirdExperience.map((skill, index) => (
                                     <SmallSkillBox
                                         key={index}
@@ -96,7 +103,7 @@ const Experience = ({ isDarkMode }) => {
                 </div>
 
                 <div className='flex flex-row w-full gap-8 h-fit'>
-                    <div className="relative w-fit  xs:h-96 sm:h-80">
+                    <div className="relative w-fit ">
                         <div className="absolute w-4 h-4 border-2 -top-1 -left-2 border-blue-950 rounded-full animate-scaleAndFade"></div>
                         <div className="absolute left-1/2 transform top-0 -translate-x-1/2 w-2 h-2 bg-blue-950 rounded-full"></div>
                         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-0.5 h-full bg-blue-950"></div>
@@ -106,10 +113,14 @@ const Experience = ({ isDarkMode }) => {
                             <h1 className='text-xl'>Luduscristaltec</h1>
                             <p>06/09/2021 - Present</p>
                         </div>
-                        <p className='mt-2'> I have acquired and sharpened my ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum vn organization. I am <span className="text-primary font-semibold">trustworthy, creative, and an effective communicator</span>. I fondly anticipate working in a challenging yet rewarding organization to attain its visions for personal growth. </p>
-                        <div className='flex flex-col w-full h-fit'>
-                            <p className='font-bold'>Technologies</p>
-                            <div className='flex flex-row w-full h-fit  p-0.5 gap-2'>
+                        <div className='flex flex-col gap-y-2'>
+                            <p className='mt-2'> I took on the role of developing the company internal website, focusing on <span className="text-primary font-semibold">front-end development</span> to improve its
+                                interactivity and visual appeal. My responsibilities involved redesigning some of its aspects and improving the user
+                                interface to create a more engaging experience to ITSector. </p>
+                            <p>This was the opportunity to further improve my knowledge going towards my second year of University.</p>
+                        </div>                        <div className='flex flex-col w-full h-fit'>
+                            <p className='font-bold mt-2'>Technologies</p>
+                            <div className='flex flex-row w-full h-fit  p-0.5 gap-2 mt-2'>
                                 {secondExperience.map((skill, index) => (
                                     <SmallSkillBox
                                         key={index}
@@ -134,10 +145,14 @@ const Experience = ({ isDarkMode }) => {
                             <h1 className='text-xl'>Luduscristaltec</h1>
                             <p>06/09/2021 - Present</p>
                         </div>
-                        <p className='mt-2'> I have acquired and sharpened my ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum vn organization. I am <span className="text-primary font-semibold">trustworthy, creative, and an effective communicator</span>. I fondly anticipate working in a challenging yet rewarding organization to attain its visions for personal growth. </p>
-                        <div className='flex flex-col w-full h-fit'>
-                            <p className='font-bold'>Technologies</p>
-                            <div className='flex flex-row flex-wrap w-fullh-fit p-0.5 gap-2'>
+                        <div className='flex flex-col gap-y-2'>
+                            <p className='mt-2'> During my time at Unviersidade da Maia, I developed a strong foundation in both theoretical and practical aspects of computer science.
+                                My coursework and projects provided me with extensive experience in <span className="text-primary font-semibold">front-end and back-end web development</span>,
+                                as well as <span className="text-primary font-semibold">mobile application development.</span></p>
+                            <p className='mt-2'> Key academic Coursework included Operating Systems, Discrete Mathematics, Object-Oriented Programming.  </p>
+                        </div>                        <div className='flex flex-col w-full h-fit'>
+                            <p className='font-bold mt-2'>Technologies</p>
+                            <div className='flex flex-row flex-wrap w-fullh-fit p-0.5 gap-2 mt-2'>
                                 {firstExperience.map((skill, index) => (
                                     <SmallSkillBox
                                         key={index}
